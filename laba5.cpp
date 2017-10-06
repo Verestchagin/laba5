04082eba9533718b36f3e7a2ae6716f8bc9c92bc
Verestchagin <32384378+Verestchagin@users.noreply.github.com>
2017-10-06 11:02:32 +0300

    Update laba5.cpp

--- laba5.cpp
+++ laba5.cpp
@@ -1,24 +1,29 @@
-  GNU nano 2.7.4                    Файл: laba5.cpp                             
-
 #include <iostream>
 using namespace std;
 
 int main(){
-        int n, m, a[n][m], b[n][m], c[n][m];
+        int n, m, ;
         cout << "Vvedite razmer massivov" << endl;
         cin >> n >> m;
+        int a[n][m], b[n][m], c[n][m];
         for (int i = 0; i < n; i++){
                 for (int j = 0; j < m; j++){
-                        cin << a[i][j];
+                        cin >> a[i][j];
                 }
-                cout << endl;
         }
         for (int i = 0; i < n; i++){
                 for (int j = 0; j < m; j++){
-                        cin << b[i][j];
+                        cin >> b[i][j];
+                }
+        }
+        for (int i = 0; i < n; i++){
+          for (int j = 0; j < m; j++){
+                        c[i][j] = a[i][j] + b[i][j];
+                        cout << c[i][j] << " ";
                 }
                 cout << endl;
         }
-           [ строка 1/29 (3%), столбец 1/20 (5%), символ 0/504 (0%) ]
-^G Помощь    ^O Записать  ^W Поиск     ^K Вырезать  ^J Выровнять ^C ТекПозиц
-^X Выход     ^R ЧитФайл   ^\ Замена    ^U Отмен. выр^T Словарь   ^_ К строке
+
+}
+
+
